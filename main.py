d2e1e0b24f7daa4bacbec52856f6c37c81591e52
DAghaSolent <5aghad30@solent.ac.uk>
2024-02-20 11:39:08 +0000

    Testing Negative linter test

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World Testing new python package Change")
+print("Changes to test new linter updates")
+if djeijeidje
+         print("Error")
+
+
+   print("More Errors")
+
+
+ weird_indent_number2=  "Negative"
+
+"Are you sure this is PEP8?"
